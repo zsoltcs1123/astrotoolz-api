0d1422d6f985361b844704f1376f6838ebd90a7f
zsoltcs <cszs1123@gmail.com>
2024-03-07 01:03:03 +0100

    configure cors

--- astrotoolz_api/main.py
+++ astrotoolz_api/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from astrotoolz_api.routers import horoscope, timeline
 
 app = FastAPI()
+
+# Define a list of allowed origins for CORS
+# For development, you might allow all origins. For production, list your frontend's origin.
+origins = [
+    "http://localhost:3000",  # Assuming your React app runs on this origin
+    "https://yourproductionfrontend.com",
+]
+
+# Add CORSMiddleware to the application instance
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows specified origins (use ["*"] for all origins)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(timeline.router)
 app.include_router(horoscope.router)
